eaecb2a5fda973a663bc34c68f78187adabf94fc
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-04-30 16:36:27 -0300

    separing

---
+++ lib/src/feactures/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:app_saude/src/feactures/login/presenter/ui/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
